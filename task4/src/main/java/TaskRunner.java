0618402690699a854aede75a255e32804de89fcb
Mikhail <levinmk23@gmail.com>
2019-11-12 14:08:51 +0300

    HomeWork 4 Реализуйте класс TaskRunner

---
+++ task4/src/main/java/TaskRunner.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TaskRunner implements Runnable{
+
+    //TODO
+
+    Object mutex;   //монитор синхронизации
+    String message; //сообщение которое поток будет добавлять в список
+
+    //у вас могут быть другие решения и другие переменные
+    volatile int cnt;
+    volatile int inc;
+    static volatile int iter = 0;
+    //этот список будет проверяться на предмет синхронизации
+    //данных
+    //а именно: [1, 2, 3, 1, 2, 3, 1, 2, 3....]
+    //в список будут писать из 3 потоков
+    static volatile LinkedList<String> list = new LinkedList<>();
+
+    public TaskRunner(Object mutex, String msg, int cnt) {
+        this.mutex = mutex;
+        message = msg;
+        this.cnt = cnt;
+        inc = 0;
+    }
+
+    @Override
+    public void run() {
+        //TODO
+    }
+
+}
---
+++ task4/src/main/java/ThreadTask.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadTask {
+
+    public void multiThreadRecord(Thread [] threads){
+        for (int i = 0; i < 3; i++) {
+            threads[i].start();
+        }
+    }
+
+}
