c7100023cdc63e852e06ca610c0a782eafb1ce0b
Mikhail <levinmk23@gmail.com>
2019-11-12 12:31:08 +0300

    HomeWork 3 Тесты работают на файлах из папки ресурсы, используйте именно эти файлы для ввода в исходном состоянии. Свои тесты делайте на копиях или собственных файлах, чтобы не изменить исходные данные) Вы должны реализовать класс FileUtility

---
+++ task3/src/main/java/chess/Bishop.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class Bishop extends ChessItem {
+    public Bishop(int x, int y, Color color) {
+        super(x, y, color);
+    }
+
+    public void move(ChessItem[][] desk, int x, int y) {
+
+    }
+}
