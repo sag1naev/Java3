385526c54a254da9e9ab510da0190d468b33e2ef
Mikhail <levinmk23@gmail.com>
2019-11-12 14:09:38 +0300

    HomeWork 4 Реализуйте класс TaskRunner

--- task4/src/main/java/ThreadTask.java
+++ task4/src/main/java/ThreadTask.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 public class ThreadTask {
 
+    // менять нельзя!
     public void multiThreadRecord(Thread [] threads){
         for (int i = 0; i < 3; i++) {
             threads[i].start();
