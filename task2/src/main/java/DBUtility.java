0f24ae4e3fa7f38978896c68438bcde59d7f0263
sag1naev <sag1naev@ya.ru>
2019-12-19 01:20:10 +0300

    Урок 2. Базы данных

--- task2/src/main/java/DBUtility.java
+++ task2/src/main/java/DBUtility.java
@@ -1,11 +1,12 @@
-
-
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class DBUtility {
     /*
-    * Каждый из тасков решается одним SQL запросом
+     * Каждый из тасков решается одним SQL запросом
      */
 
     /*
@@ -18,22 +19,37 @@ public class DBUtility {
      */
 
 
-    void AddPrinters(Statement stmt){
-        // TODO: 16.12.2019  
+    void AddPrinters(Statement stmt) throws SQLException {
+        insertNewValue(stmt, 1012, "col", "laser", 20000, "HP");
+        insertNewValue(stmt, 1010, "bw", "jet", 5000, "Canon");
+        insertNewValue(stmt, 1010, "bw", "jet", 5000, "Canon");
     }
 
 
-    public void createPrinterTable(Connection con, Statement  stmt){
-        // TODO: 16.12.2019  
+    public void createPrinterTable(Connection con, Statement stmt) throws SQLException {
+        stmt.execute("CREATE TABLE IF NOT EXISTS  Printer (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, model INTEGER, color TEXT, type TEXT, price INTEGER);");
+        AddPrinters(stmt);
+    }
+
+    private void insertNewValue(Statement stmt, int model, String color, String type, int price, String maker) throws SQLException {
+        stmt.execute("INSERT INTO Printer(model, color, type , price) VALUES (" + model + ", '" + color + "', '" + type + "', " + price + ");");
+        stmt.execute("INSERT INTO Product(maker, model, type) VALUES ('" + maker + "', " + model + ", '" + type + "');");
     }
 
     /*
-    * Метод должен вернуть список уникальных моделей PC дороже 15 тысяч
+     * Метод должен вернуть список уникальных моделей PC дороже 15 тысяч
      */
 
-    public ArrayList<String> selectExpensivePC(Statement stmt){
-        //todo
-        return null;
+    public ArrayList<String> selectExpensivePC(Statement stmt) {
+        ArrayList<String> result = new ArrayList<>();
+        try (ResultSet expensiveModelPC = stmt.executeQuery("SELECT DISTINCT model FROM PC WHERE price > 15000;")) {
+            while (expensiveModelPC.next()) {
+                result.add(expensiveModelPC.getString("model"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     /*
@@ -41,18 +57,32 @@ public class DBUtility {
      * которых выше чем 2500
      */
 
-    public ArrayList<Integer> selectQuickLaptop(Statement stmt){
-        // TODO: 16.12.2019  
-        return null;
+    public ArrayList<Integer> selectQuickLaptop(Statement stmt) {
+        ArrayList<Integer> result = new ArrayList<>();
+        try (ResultSet expensiveModelPC = stmt.executeQuery("SELECT id FROM Laptop WHERE speed > 2500;")) {
+            while (expensiveModelPC.next()) {
+                result.add(expensiveModelPC.getInt("id"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     /*
      * Метод должен вернуть список производителей которые
      *  делают и пк и ноутбуки
      */
-    public ArrayList<String> selectMaker(Statement stmt){
-        //todo
-        return null;
+    public ArrayList<String> selectMaker(Statement stmt) {
+        ArrayList<String> result = new ArrayList<>();
+        try (ResultSet expensiveModelPC = stmt.executeQuery("SELECT distinct maker FROM Product WHERE type IN ('PC', 'Laptop') GROUP BY type;")) {
+            while (expensiveModelPC.next()) {
+                result.add(expensiveModelPC.getString("maker"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     /*
@@ -64,10 +94,14 @@ public class DBUtility {
      * или сделать любым другим способом
      */
 
-    public int makerWithMaxProceeds(Statement stmt){
-        int result = 0;
-        //todo
-        return result;
-
+    public int makerWithMaxProceeds(Statement stmt) {
+        try (ResultSet expensiveModelPC = stmt.executeQuery("SELECT SUM(price) as total FROM ( SELECT model, price FROM Laptop UNION SELECT model, price FROM PC ) as u LEFT JOIN Product ON u.model = Product.model GROUP BY maker ORDER BY total DESC LIMIT 1;")) {
+            if (expensiveModelPC.next()) {
+                return expensiveModelPC.getInt("total");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 }
